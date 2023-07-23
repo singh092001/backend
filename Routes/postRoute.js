42d3208ea842ad63f5427f2ce05d3f26d03e6d55
Chakresh2001 <jha.chakresh2001@gmail.com>
2023-07-24 01:33:02 +0530

    done

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -3,11 +3,48 @@ const { userAuth } = require("../Middlewares/user.auth");
 const postModel = require("../Model/PostModel");
 
 
+
+// cat_name
+// "Whiskers"
+// age
+// 2
+// breed
+// "Domestic Shorthair"
+// color
+// "Tabby"
+// gender
+// "Male"
+// size
+// "Medium"
+// description
+// "Whiskers is a friendly and playful two-year-old male cat. He has a bea…"
+// adoption_fee
+// "$50"
+// location
+// "los angeles,california"
+// image_url
+
+
 const postRouter=express.Router();
 
     postRouter.post("/add",userAuth,async(req,res)=>{
         try {
            
+           if(req.body.type=="cat"){
+            const obj={
+                cat_name:req.body.cat_name,
+                age:req.body.age,
+                breed:req.body.breed,
+                location:req.body.location,
+                image_image:req.body.image_image,
+                adoption_price:req.body.adoption_price,
+
+                userID:req.body.userID,
+                userName:req.body.userName,
+                user_location:req.body.user_location
+            }
+           }
+           else{
             const obj={
                 pet_name:req.body.pet_name,
                 pet_breed:req.body.pet_breed,
@@ -20,8 +57,10 @@ const postRouter=express.Router();
                 userName:req.body.userName,
                 user_location:req.body.user_location
             }
-            const userpost=new postModel(obj);
-                await userpost.save()
+            console.log(obj)
+           }
+            const userpost= postModel(obj);
+            await userpost.save()
 
                 res.status(200).json({msg:"Pet Added successfully",userpost})
         
